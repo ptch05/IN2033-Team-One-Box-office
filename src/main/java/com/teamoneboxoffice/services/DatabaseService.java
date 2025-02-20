854587b68ad43a9113e287be84461571d41a9e53
Peiman Timaji <peimantimaji@yahoo.com>
2025-02-20 02:26:24 +0000

    Connected to database and also allows multiple users, details pre-defined in config.properties

--- src/main/java/com/teamoneboxoffice/services/DatabaseService.java
+++ src/main/java/com/teamoneboxoffice/services/DatabaseService.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 //Loads database configuration from config.properties file
@@ -11,23 +13,39 @@ import java.util.Properties;
 
 public class DatabaseService {
     private static String URL;
-    private static String USER;
-    private static String PASSWORD;
+
+    // Map of username to password
+    private static Map<String, String> userCredentials = new HashMap<>();
 
     static {
         try {
+            // Load database configuration from config.properties
             Properties properties = new Properties();
-            properties.load(DatabaseService.class.getClassLoader().getResourceAsStream("config.properties"));
+            properties.load(new FileInputStream("config.properties"));
             URL = properties.getProperty("db.url");
-            USER = properties.getProperty("db.user");
-            PASSWORD = properties.getProperty("db.password");
+
+            // Load multiple users into the map
+            userCredentials.put(properties.getProperty("db.user1.username"), properties.getProperty("db.user1.password"));
+            userCredentials.put(properties.getProperty("db.user2.username"), properties.getProperty("db.user2.password"));
+            userCredentials.put(properties.getProperty("db.user3.username"), properties.getProperty("db.user3.password"));
+
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to load database configuration");
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    // Returns a connection to the database if the username and password are valid
+    public static Connection getConnection(String username, String password) throws SQLException {
+        if (userCredentials.containsKey(username) && userCredentials.get(username).equals(password)) {
+            return DriverManager.getConnection(URL, username, password);
+        } else {
+            throw new SQLException("Invalid username or password");
+        }
+    }
+
+    // Returns the password for a given username
+    public static String getPasswordForUser(String username) {
+        return userCredentials.get(username);
     }
 }
